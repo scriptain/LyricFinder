52b178bd969d64f207f88ef2a4a2b7e278c4a350
scriptain <scriptain@gmail.com>
2024-06-14 13:44:52 +1000

    Program now loops through each song across all albums in Library/Beatles directory. Need to now search within each txt file for the given lyric and store each match in a list or array.

--- LyricFinder/Program.cs
+++ LyricFinder/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true) 
+            {
+                Console.WriteLine("Enter a lyric to search:");
+                var input = Console.ReadLine(); 
+                Search(input);
+            }
         }
+
+        // Search() takes in a search term as input and searches the ./Library/Beatles directory
+        // may expand to other bands/artists down the track
+        static string Search(string searchTerm)
+        {
+            // @ signifies 'verbatim string' ignoring escape sequences 
+            string libraryPath = @"C:\Users\scrip\source\repos\LyricFinder\LyricFinder\Library\Beatles";
+
+            if (Directory.Exists(libraryPath))
+            {
+                // capture/store the contents of each .txt file in the 'libraryPath' directory  
+                string[] files = Directory.GetFiles(libraryPath, "*.txt", SearchOption.AllDirectories);
+
+                foreach (string file in files)
+                {
+                    // at this point 'file' is a string that contains the full path of every song across all albums
+                    Console.WriteLine($"{file}");
+
+                }
+            }
+            else
+            {
+                Console.Write("Directory Doesn't Exist");
+            }
+
+            return searchTerm;
+        }
+
+
+
     }
 }
