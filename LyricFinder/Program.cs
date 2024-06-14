faf04b72c6a50c80f2934682afc0738f260c3082
scriptain <scriptain@gmail.com>
2024-06-14 19:37:35 +1000

    Created a constructor for the struct. each match now prints out the expected information. where a match is found in a chorus, each unique line is printed out resulting in multiple matches for the same song. can probably fix this by filtering out some results. leaving it as is for now.

--- LyricFinder/Program.cs
+++ LyricFinder/Program.cs
@@ -8,18 +8,24 @@
             {
                 Console.WriteLine("Enter a lyric to search:");
                 var input = Console.ReadLine();
-                string[] matches = Search(input);
-                Console.WriteLine(matches.Length);
+                List<Match> matches = Search(input);
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    Console.WriteLine(match.ToString());
+                    Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                }
             }
         }
 
         // Search() takes in a search term as input and searches the ./Library/Beatles directory
         // may expand to other bands/artists down the track
-        static string[] Search(string searchTerm)
+        static List<Match> Search(string searchTerm)
         {
             // @ signifies 'verbatim string' ignoring escape sequences 
             string libraryPath = @"C:\Users\scrip\source\repos\LyricFinder\LyricFinder\Library\Beatles";
-            string[] matches = Array.Empty<string>();
+            // a list of matches that can be iterated through later
+            List<Match> matches = new List<Match>();
             if (Directory.Exists(libraryPath))
             {
                 // capture/store the contents of each .txt file in the 'libraryPath' directory  
@@ -27,7 +33,16 @@
 
                 foreach (string file in files)
                 {
-                    // at this point 'file' is a string that contains the full path of every song across all albums
+                    // 'file' is a path that resembles C:\x\x\x\beatles\somealbum\somesong.txt
+                    // Extract the name of the album and the name of the song to use later
+
+                    // create a new instance of DirectoryInfo class
+                    DirectoryInfo dirInfo = new DirectoryInfo(file);
+
+                    // in this case the directory "Name" will be the title of the song "Run_For_Your_Life.txt"
+                    string songName = dirInfo.Name;
+                    string album = dirInfo.Parent.Name;
+                    string artist = dirInfo.Parent.Parent.Name;
                     // wrapping in try catch block so as to 'gracefully' handle various errors such as filenotfound, access denied, disk errors
                     try
                     {
@@ -36,16 +51,12 @@
 
                         // go through the song line by line
                         for (int i = 0; i < lines.Length; i++)
-                        {
+                        {   
                             // check if this line is a match
                             if (lines[i].Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
                             {
-                                Console.WriteLine($"Found a match at line: {i}");
-                                Console.WriteLine(lines[i]);
-                                Console.WriteLine(file);
-                                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-
-
+                                // construct Match 
+                                matches.Add(new Match(artist, album, songName, lines[i], i));
                             }
                         }
                     }
@@ -70,10 +81,26 @@
             // {get; set;} are 'accessors' for the property. They define how the property can be accessed and modified
             // get: returns the value of the property. Match.artist is called
             // set: assigns a value to the property. Match.artist = "Bono" uses set implicitly
-            public string artist { get; set; }
-            public string album { get; set; }
-            public string songTitle { get; set; }
-            public int line { get; set; }
+            public string Artist { get; }
+            public string Album { get; }
+            public string SongTitle { get; }
+            public string Line { get; }
+
+            public int LineNum { get; }
+
+            public Match(string artist, string album, string songTitle, string line, int lineNum)
+            {
+                Artist = artist;
+                Album = album;
+                SongTitle = songTitle;
+                LineNum = lineNum;
+                Line = line;
+            }
+
+            public override string ToString()
+            {
+                return $"Artist: {Artist} \n Album: {Album} \n Song: {SongTitle} \n Found at line: {LineNum} \n Full Line: {Line}";
+            }
         }
     }
 }
