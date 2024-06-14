0e7f2e07151019a8885dc3ea4ec0277ca2e31acc
scriptain <scriptain@gmail.com>
2024-06-14 15:03:47 +1000

    search() finds appropriate matches. Problem is that despite the number of matches found, matches.Append(file).ToArray() fails to add the song to the matches array. Going to save for now and change search() so it searches line by line so as to capture the line at which the lyric was found. Will also refactor so matches are saved to "Match" data tructure that has the following properties: "line" for the line where the lyric was found, "artist", and "album"

--- LyricFinder/Program.cs
+++ LyricFinder/Program.cs
@@ -8,17 +8,18 @@
             {
                 Console.WriteLine("Enter a lyric to search:");
                 var input = Console.ReadLine(); 
-                Search(input);
+                string[] matches = Search(input);
+                Console.WriteLine(matches.Length);
             }
         }
 
         // Search() takes in a search term as input and searches the ./Library/Beatles directory
         // may expand to other bands/artists down the track
-        static string Search(string searchTerm)
+        static string[] Search(string searchTerm)
         {
             // @ signifies 'verbatim string' ignoring escape sequences 
             string libraryPath = @"C:\Users\scrip\source\repos\LyricFinder\LyricFinder\Library\Beatles";
-
+            string[] matches = Array.Empty<string>();
             if (Directory.Exists(libraryPath))
             {
                 // capture/store the contents of each .txt file in the 'libraryPath' directory  
@@ -27,7 +28,22 @@
                 foreach (string file in files)
                 {
                     // at this point 'file' is a string that contains the full path of every song across all albums
-                    Console.WriteLine($"{file}");
+                    // wrapping in try catch block so as to 'gracefully' handle various errors such as filenotfound, access denied, disk errors
+                    try
+                    {
+                        // store the lyrics in "content"
+                        string content = File.ReadAllText(file);
+                        // perform a case insensitive search for searchTerm
+                        if(content.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
+                        {
+                            // Match found, add it to matches array 
+                            matches.Append(file).ToArray();
+                            Console.WriteLine(file);
+                        }
+                    } catch (Exception ex)
+                    {
+                    Console.WriteLine(ex.Message); 
+                    }
 
                 }
             }
@@ -35,8 +51,11 @@
             {
                 Console.Write("Directory Doesn't Exist");
             }
-
-            return searchTerm;
+            foreach(string match in matches) 
+             {
+                Console.WriteLine(match);
+             }
+            return matches;
         }
 
 
